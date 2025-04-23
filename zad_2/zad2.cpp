0ffeeef1ceb72ea24bfa59326c7f868d1b5cafc0
marcinmik10 <marcin2003@outlook.com>
2025-04-23 18:57:13 +0200

    zad2

--- zad_2/zad2.cpp
+++ zad_2/zad2.cpp
@@ -18,21 +18,6 @@ double force(double x) {
     return -(potential(x + dx) - potential(x - dx)) / (2 * dx);
 }
 
-// Metoda Verleta
-// void verlet(std::ofstream &file) {
-//     double x = 2.8, v = 0.0;
-//     double a = force(x) / m;
-//     double dt = 0.001; //krok czasowy
-//     for (double t = 0; t <= t_max; t += dt) {
-//         double x_new = x + v * dt + 0.5 * a * dt * dt;
-//         double a_new = force(x_new) / m;
-//         double v_new = v + 0.5 * (a + a_new) * dt;
-//         x = x_new;
-//         v = v_new;
-//         a = a_new;
-//         file << t << " " << x << " " << v << std::endl;
-//     }
-// }
 
 void verlet(std::ofstream &file) {
     double x = 2.8, v = 0.0, t = 0.0;
@@ -73,30 +58,6 @@ void verlet(std::ofstream &file) {
         // if (t + 2 * dt > t_max) dt = (t_max - t) / 2.0;
     }
 }
-
-
-// Metoda RK4
-// void rk4(std::ofstream &file) {
-//     double x = 2.8, v = 0.0;
-//     double dt = 0.01; //krok czasowy
-//     for (double t = 0; t <= t_max; t += dt) {
-//         double k1_x = v;
-//         double k1_v = force(x) / m;
-
-//         double k2_x = v + 0.5 * dt * k1_v;
-//         double k2_v = force(x + 0.5 * dt * k1_x) / m;
-
-//         double k3_x = v + 0.5 * dt * k2_v;
-//         double k3_v = force(x + 0.5 * dt * k2_x) / m;
-
-//         double k4_x = v + dt * k3_v;
-//         double k4_v = force(x + dt * k3_x) / m;
-
-//         x += dt / 6.0 * (k1_x + 2 * k2_x + 2 * k3_x + k4_x);
-//         v += dt / 6.0 * (k1_v + 2 * k2_v + 2 * k3_v + k4_v);
-//         file << t << " " << x << " " << v << std::endl;
-//     }
-// }
 
 void rk4(std::ofstream &file) {
     double x = 2.8, v = 0.0, t = 0.0;
